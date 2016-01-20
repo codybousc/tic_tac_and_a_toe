65a910582fccd59975a72d91a005901c8dd12ece
Cody Bouscaren <codybousc@gmail.com>
2016-01-20 17:18:07 -0500

    Board and square availability function set

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -2,6 +2,45 @@ ticTacAndAToe = angular.module("ticTacAndAToe", [])
 
 ticTacAndAToe.controller('GameController', ['$scope','$http',
 function($scope, $http) {
-  $scope.words = "Getting goin'"
 
+  $scope.board = {
+    "A1": "X",
+    "A2": " ",
+    "A3": " ",
+    "B1": " ",
+    "B2": " ",
+    "B3": " ",
+    "C1": " ",
+    "C2": " ",
+    "C3": " "
+  }
+
+  $scope.player = {
+    "name": " ",
+    "Marker": " "
+  }
+
+  $scope.computer = {
+    "name": "R2D2",
+    "marker": " "
+  }
+
+  $scope.squareAvailability = function(specificKey) {
+    var boardLength = Object.keys($scope.board).length;
+    var boardKeys = Object.keys($scope.board);
+    for(var key in $scope.board) {
+      if($scope.board[specificKey] == " ") {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+
+  $scope.squareAvailability("A1");
+
+  $scope.pickSquare = function() {
+
+  }
 }]);
