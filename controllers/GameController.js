4d9b2007840968fd108222a73897ce2d1fd6071e
Cody Bouscaren <codybousc@gmail.com>
2016-01-20 15:31:44 -0500

    Initial file structure set

---
+++ controllers/GameController.js
@@ -0,0 +1,7 @@
+ticTacAndAToe = angular.module("ticTacAndAToe", [])
+
+ticTacAndAToe.controller('GameController', ['$scope','$http',
+function($scope, $http) {
+  $scope.words = "Getting goin'"
+
+}]);
