40d3ed19f5536572915eae978531ef86c40d9174
Cody Bouscaren <codybousc@gmail.com>
2016-01-22 16:08:12 -0500

    Tie functionality added

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -8,14 +8,14 @@ function($scope, $http) {
   $scope.computerMarker;
 
   $scope.board = {
-    "A1": " ",
-    "A2": " ",
-    "A3": " ",
-    "B1": " ",
-    "B2": " ",
+    "A1": "X",
+    "A2": "O",
+    "A3": "X",
+    "B1": "O",
+    "B2": "X",
     "B3": " ",
-    "C1": " ",
-    "C2": " ",
+    "C1": "X",
+    "C2": "O",
     "C3": " "
   }
 
@@ -46,11 +46,9 @@ function($scope, $http) {
   $scope.playersChoice = function() {
     $scope.$chosenSquare = prompt("Enter an available square, bro");
     $scope.playerPickSquare($scope.$chosenSquare, $scope.playerMarker)
-
   }
 
   $scope.squareAvailability = function(specificKey) {
-    console.log("ARGS LEngth ", arguments.length);
     //Check Square Availability for Player Move (argument provided)
     if(arguments.length > 0) {
       var boardKeys = Object.keys($scope.board);
@@ -99,14 +97,13 @@ function($scope, $http) {
   $scope.playerPickSquare = function(chosenSquare, playerMarker) {
     if($scope.squareAvailability(chosenSquare)) {
       $scope.board[chosenSquare] = playerMarker;
-      console.log("pickSquare ", $scope.board);
     }
     else {
       console.log("That's square's already taken, my friend");
     }
   }
 
-// TODO right now, this is just iterating through the object and placing the marker in numberical order. not exactly random. also doesn't choose if site is already taken
+// TODO right now, this is just iterating through the object and placing the marker in numberical order. not exactly random.
   $scope.randoComputerMove = function() {
     var computerMarker = $scope.computerMarker;
     var boardKeys = Object.keys($scope.board); //returns an array of $scope.board keys
@@ -114,25 +111,15 @@ function($scope, $http) {
     for(var i = 0; i < boardLength; i++) {
       //if space is not occupied
       if($scope.squareAvailability(boardKeys[i])) {
-        console.log("PASSING FIRST CONDITIONAL");
-        // console.log("KEY = ", boardKeys[i]);
         $scope.board[boardKeys[i]] = computerMarker;
-        // console.log($scope.squareAvailability(key), $scope.board[key]);
-        // counter+=1
-        // console.log(counter);
-        console.log("randoComputerMove ", $scope.board)
         break
       }
       //if space is occupied
       else {
         while(!$scope.squareAvailability(boardKeys[i])) {
           i++;
-          console.log("I is = to ", i)
           if($scope.squareAvailability(boardKeys[i])) {
-            console.log("PASSING SECOND CONDITIONAL");
-            console.log(boardKeys[i], "= ", computerMarker);
             $scope.board[boardKeys[i]] = computerMarker;
-            console.log("randoComputerMove ", $scope.board);
             break
           }
         }
@@ -142,6 +129,8 @@ function($scope, $http) {
   }
 
   $scope.checkForWinner = function() {
+    var openSpaces = [];
+    var winner = false;
     //Check if winning lines are occupied && occupied by the same marker
     var boardLength = Object.keys($scope.board).length;
     var threeInARow = [["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"], ["A1", "B1", "C1"], ["A2", "B2", "B3"], ["C1", "C2", "C3" ], ["A1", "B2", "C3"], ["C1", "B2", "A3"]];
@@ -149,13 +138,28 @@ function($scope, $http) {
     var everyThirdInc = 0;
     var nopeCounter = 0;
     for(var i = 0; i < threeInARow.length; i++) {
-      // console.log("everyThirdInc = ", everyThirdInc);
-      // console.log("COUNTER = ", counter);
-      // console.log("SOME SHIZ ", threeInARow[everyThirdInc][counter])
       if($scope.board[threeInARow[everyThirdInc][counter]] == "X" && $scope.board[threeInARow[everyThirdInc][counter + 1]] == "X" && $scope.board[threeInARow[everyThirdInc][counter + 2]] == "X"
       || $scope.board[threeInARow[everyThirdInc][counter]] == "O" && $scope.board[threeInARow[everyThirdInc][counter + 1]] == "O" && $scope.board[threeInARow[everyThirdInc][counter + 2]] == "O") {
+        winner = true;
           console.log("We've got ourselves a weiner!");
           break
+        }
+        //Tie (All Squares occupied and no winner)
+        else if (winner == false){
+          var newCounter = 0;
+          for(var key in $scope.board) {
+            newCounter++;
+            console.log("HOW MANY TIMES AM I GETTING CALLED??? ", newCounter);
+            var value = $scope.board[key];
+            if(value == " ") {
+              openSpaces.push(value)
+              console.log(key, " is available")
+            }
+          }
+          if(openSpaces.length == 0) {
+            console.log("It's a tie!");
+            break
+          }
         }
       else {
         console.log("NOPE COUNTER ", nopeCounter);
@@ -164,23 +168,21 @@ function($scope, $http) {
       }
     }
   }
-  $scope.choseMarker();
-  $scope.playersChoice();
-  $scope.randoComputerMove();
-
-  $scope.playersChoice();
-  $scope.randoComputerMove();
-
-  $scope.playersChoice();
+  // $scope.choseMarker();
+  // $scope.playersChoice();
+  // $scope.randoComputerMove();
+  //
+  // $scope.playersChoice();
+  // $scope.randoComputerMove();
+  //
+  // $scope.playersChoice();
+  // $scope.checkForWinner();
+  // $scope.randoComputerMove();
+  // $scope.checkForWinner();
+  //
+  // $scope.playersChoice();
+  // $scope.checkForWinner();
+  // $scope.randoComputerMove();
   $scope.checkForWinner();
-  $scope.randoComputerMove();
-  $scope.checkForWinner();
-
-  $scope.playersChoice();
-  $scope.checkForWinner();
-  $scope.randoComputerMove();
-  $scope.checkForWinner();
-
-
 
 }]);
